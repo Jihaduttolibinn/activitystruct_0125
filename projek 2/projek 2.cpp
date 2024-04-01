127a1cd0a2c815d696c05c9a7fccd5013ca3237f
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-01 17:59:28 +0800

    struct DetailAlamat

---
+++ projek 2/projek 2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kkota;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
