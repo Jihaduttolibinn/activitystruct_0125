d96a84d3519bab9f1db0c3dbfd2597d01208f2d9
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-01 18:55:58 +0800

    Alamat Mahasiswa

--- projeck 3/projeck 3.cpp
+++ projeck 3/projeck 3.cpp
@@ -21,7 +21,9 @@ int main() {
     getline(cin, mhs.nim);
     cout << "Nama Mahasiswa : ";
     getline(cin, mhs.nama);
-
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
 
 
 
