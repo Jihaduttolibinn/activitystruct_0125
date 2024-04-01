f7a26b221f14aeb8e353d35a92ef000a95db9c95
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-01 17:56:29 +0800

    int main

--- activitystruct_125/activitystruct_125.cpp
+++ activitystruct_125/activitystruct_125.cpp
@@ -9,13 +9,23 @@ struct Mahasiswa {
     int umur;
 };
 
-
-
-
-
-
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim : " << mhs.nim;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+
 }
 
